0586bfb23b660ac66a5867befc4945a0fc377419
IshaSingh0228 <78353412+IshaSingh0228@users.noreply.github.com>
2023-03-17 18:27:44 +0530

    Update variables.tf

--- VaultExample/modules/vault_auth/variables.tf
+++ VaultExample/modules/vault_auth/variables.tf
@@ -2,10 +2,10 @@ variable "region" {
   default = "us-east-1"
 }
 variable "access_key" {
-  default = "AKIARNPRINAO3CFF3JSE"
+  default = "AK***********E"
 }
 variable "secret_key" {
-  default = "+SBcwIwgk9dpwJNb/q7ruZbXVg/IFVNDZEOTZM2P" 
+  default = "*********************P" 
 }
 variable "vault_addr" {
   default = "http://127.0.0.1:8200"
